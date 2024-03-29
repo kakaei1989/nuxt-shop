58ef5f8935dfd486831e8b7f39efcde3d4c9c280
Ahmad Kakaei <kakaei1989@gmail.com>
2024-03-29 17:42:08 +0330

    create formkit form

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
     },
     devtools: {enabled: true},
     css: ['~/assets/css/main.css'],
+    modules : ["@formkit/nuxt"],
     build: {
         transpile: ['vue-toastification'],
     }
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,19 +1,39 @@
 <template>
     <div class="vh-70">
-        <div class="row g-4">
-            <div class="col col-md-6">
-                <label class="form-label">نام و نام خانوادگی</label>
-                <input type="text" class="form-control" value="لورم ایپسوم">
+        <FormKit type="form" @submit="edit" :incomplete-message="false" :actions="false">
+            <div class="row g-4">
+                <div class="col col-md-6">
+                    <FormKit type="text" name="name" id="name" label="نام و نام خانوادگی" label-class="form-label"
+                        input-class="form-control" validation="required"
+                        :validation-messages="{ required: 'فیلد نام و نام خانوادگی الزامیست' }"
+                        messages-class="form-text text-danger" />
+                </div>
+
+                <div class="col col-md-6">
+                    <FormKit type="email" name="email" id="name" label="ایمیل" label-class="form-label"
+                        input-class="form-control" validation="required|email"
+                        :validation-messages="{ required: 'فیلد ایمیل الزامیست', email: 'فرمت فیلد معتبر نیست' }"
+                        messages-class="form-text text-danger" />
+                </div>
+
+                <div class="col col-md-6">
+                    <label class="form-label">شماره تلفن</label>
+                    <input type="text" disabled class="form-control" value="09100000000">
+                </div>
+
+                <FormKit type="submit" input-class="btn btn-primary mt-4">
+                    ویرایش
+                </FormKit>
             </div>
-            <div class="col col-md-6">
-                <label class="form-label">ایمیل</label>
-                <input type="text" class="form-control" value="test@gmail.com">
-            </div>
-            <div class="col col-md-6">
-                <label class="form-label">شماره تلفن</label>
-                <input type="text" disabled class="form-control" value="09100000000">
-            </div>
-        </div>
-        <button type="submit" class="btn btn-primary mt-4">ویرایش</button>
+        </FormKit>
     </div>
 </template>
+
+<script setup>
+
+function edit(formDate) {
+
+    console.log(formDate);
+}
+
+</script>
